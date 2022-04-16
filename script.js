9374060a39bf91bf9640fc42ff9f2abfe16f468c
MIke <williamrijkens@outlook.com>
2022-04-16 15:57:38 +0200

    fixing duplicate code

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 'use strict';
 
-// const randomNumber = Math.trunc(Math.random()*20) +1;
+// for test 
+// const randomNumber = 1;
 let randomNumber = Math.trunc(Math.random()*20) +1;
  
 const btn = document.querySelector('.hidden');
 const pageBody = document.querySelector('body');
+const displayMessage = function(message){
+    document.querySelector('.number-guess').textContent = message;
+}
 
 document.querySelector('.btn').addEventListener('click', function(){
     const guess = Number(document.querySelector('.guess-number').value);
     if(!guess){
-        document.querySelector('.number-guess').textContent = 'Plss use a number!';
+        displayMessage('Plss use a number!!!');
     }else if(guess === randomNumber){
-        document.querySelector('.number-guess').textContent = `--- ${randomNumber} --- you have guess the number!`;
+        displayMessage(`--- ${randomNumber} --- you have guess the number!`);
         document.querySelector('body').style.backgroundColor = '#06113C';
         document.querySelector('body').style.color = '#FFFBE7';
         buttonApear();
         document.querySelector('.message').textContent = 'We have a Winner!!';
-    }else{
-        document.querySelector('.number-guess').textContent = 'Wrong Number';
+    }else if (guess !== randomNumber){
+        displayMessage(guess > randomNumber ? 'To Hight' : 'To Low');  
     }
 });
 
@@ -26,8 +30,9 @@ function buttonApear(){
     btn.style.visibility = 'visible';
 }
 
+
 btn.addEventListener('click', function() {
-    document.querySelector('.number-guess').textContent = 'Guess Random Number:';
+    displayMessage('Guess Random Number:');
     document.querySelector('.message').textContent = 'Guess The Number!';
     document.querySelector('body').style.backgroundColor = 'rgb(53, 48, 48)';
     document.querySelector('body').style.color = 'rgb(255, 255, 255)';
